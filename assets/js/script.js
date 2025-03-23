6113ec632c3685573f156aa1774061eb8cf7a3c0
ryy26 <138737653+ryy26@users.noreply.github.com>
2025-03-23 10:33:22 +0700

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,66 @@
+AOS.init()
+
+lightbox.option({
+	'alwaysShowNavOnTouchDevices': true,
+	'wrapAround': true
+})
+
+var musik = ''
+var audio = document.querySelector('.audio')
+if (musik) 
+{
+	audio.src = musik
+}
+
+function mulai() 
+{
+	audio.play()
+	window.scrollTo(0, 0)
+	var mail_section = $('#mail-section')
+	$('#mail').attr('src', 'assets/img/mail.gif')
+	setTimeout(function() {
+		mail_section.css('opacity', 0)
+		$('body').removeClass('overflow-hidden')
+	}, 2000)
+	setTimeout(function() {
+		mail_section.removeClass('d-flex')
+		mail_section.addClass('d-none')
+	}, 4000)
+}
+
+function wa(isi) 
+{
+	open("https://wa.me/6282228386077?text=hai" + isi)
+}
+
+async function makeawish() 
+{
+	var {
+		value: kado
+	} = await swal.fire({
+		imageUrl: 'assets/img/wuih.png',
+		title: 'Di hari spesial ini, kamu mau apa?',
+		imageWidth: 400,
+		imageHeight: 400,
+		confirmButtonColor: '#707372',
+		confirmButtonText: 'Kirim ✈',
+		input: 'text',
+		showCancelButton: false
+	})
+
+	if (kado) {
+		await swal.fire({
+			imageUrl: 'assets/img/ayaya.png',
+			title: 'Kirim jawabannya ke wa aku ya?',
+			confirmButtonColor: '#707372',
+			confirmButtonText: 'Kirim ✈'
+		})
+		wa(kado)
+	} else {
+		await swal.fire({
+			icon: 'error',
+			title: 'Jangan dikosongin dong!!!'
+		})
+		makeawish()
+	}
+}
